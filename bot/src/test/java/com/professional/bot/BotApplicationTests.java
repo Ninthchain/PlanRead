c08a7ce588dc1d97728e5930bc4799c87e4a1482
Ninthchain <ninthchainOFF@gmail.com>
2025-01-20 00:59:25 +0300

    first commit

---
+++ bot/src/test/java/com/professional/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.professional.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = BotApplication.class)
+class BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
