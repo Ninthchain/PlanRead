40b6ed547209e3ebafb1a067b52fc1732a67a47b
Ninthchain <ninthchainOFF@gmail.com>
2025-01-21 02:38:38 +0300

    Added id column

--- backend/src/main/java/com/professional/backend/model/dao/UserTaskTag.java
+++ backend/src/main/java/com/professional/backend/model/dao/UserTaskTag.java
@@ -1,8 +1,15 @@
 package com.professional.backend.model.dao;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class UserTaskTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     
 }
