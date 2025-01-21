6a96ff55805558fe23be2f6db64f56972192194c
Ninthchain <ninthchainOFF@gmail.com>
2025-01-22 01:29:47 +0300

    Added many-to-many relationship with UserTask

--- backend/src/main/java/com/professional/backend/model/dao/UserTaskTag.java
+++ backend/src/main/java/com/professional/backend/model/dao/UserTaskTag.java
@@ -1,9 +1,13 @@
 package com.professional.backend.model.dao;
 
+import java.util.Set;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class UserTaskTag {
@@ -12,4 +16,9 @@ public class UserTaskTag {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
+    @Column
+    private String name;
+
+    @ManyToMany
+    private Set<UserTask> tasks;
 }
