f6d61bfa9455425b4b5d41d022068c58b08f29bd
Ninthchain <ninthchainOFF@gmail.com>
2025-01-22 23:21:56 +0300

    Moved to com.professional.backend.model.dao.task package(folder)

---
+++ backend/src/main/java/com/professional/backend/model/dao/task/UserTask.java
@@ -0,0 +1,38 @@
+package com.professional.backend.model.dao.task;
+
+import java.util.Set;
+import java.util.UUID;
+
+import com.professional.backend.model.dao.Tag;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "tasks")
+public class UserTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "task_name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "priority_id", referencedColumnName = "id")
+    private UserTaskPriority priority;
+
+    @ManyToMany
+    private Set<Tag> tags;
+}
---
+++ backend/src/main/java/com/professional/backend/model/dao/task/UserTaskPriority.java
@@ -0,0 +1,22 @@
+package com.professional.backend.model.dao.task;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "task_priorities")
+public class UserTaskPriority {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Short id;
+
+    @Column(name = "index")
+    private Integer index;
+}
