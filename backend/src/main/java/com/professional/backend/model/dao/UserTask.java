13f685b596d066deb2b17bd2fdfda767cba3394a
Ninthchain <ninthchainOFF@gmail.com>
2025-01-22 23:09:30 +0300

    Added getters and setters

--- backend/src/main/java/com/professional/backend/model/dao/UserTask.java
+++ backend/src/main/java/com/professional/backend/model/dao/UserTask.java
@@ -12,7 +12,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity(name = "tasks")
 public class UserTask {
 
