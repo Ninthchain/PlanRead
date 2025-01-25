cb8a9fc12d3af4c0c4eb8937e5b3169c32eb8895
Ninthchain <ninthchainOFF@gmail.com>
2025-01-26 01:27:37 +0300

    Added table index to search easier by name

--- backend/src/main/java/com/professional/backend/infrastructure/data/model/dao/task/Tag.java
+++ backend/src/main/java/com/professional/backend/infrastructure/data/model/dao/task/Tag.java
@@ -7,22 +7,25 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity(name = "tags")
+@Table(indexes = @Index(columnList = "name", name = "name_idx"))
 public class Tag {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
+
     @Column
     private String name;
 
-    @ManyToMany
+    @ManyToMany(mappedBy = "tags")
     private Set<UserTask> tasks;
 }
