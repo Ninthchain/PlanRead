c5f217cafbd21818b98a7946353c7d94f195a7c8
Ninthchain <ninthchainOFF@gmail.com>
2025-01-26 01:26:14 +0300

    Entity now have its own id (UUID class). Made telegram username not null column. Added relationship with books table (one-to-many, non required)

--- backend/src/main/java/com/professional/backend/infrastructure/data/model/dao/user/User.java
+++ backend/src/main/java/com/professional/backend/infrastructure/data/model/dao/user/User.java
@@ -1,14 +1,29 @@
 package com.professional.backend.infrastructure.data.model.dao.user;
 
+import java.util.Set;
+import java.util.UUID;
+
+import com.professional.backend.infrastructure.data.model.dao.book.Book;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class User {
+    
     @Id
-    @Column(name = "telegram_username")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "telegram_username", nullable = false)
     private String username;
 
-    
+    @OneToMany
+    @JoinColumn(name = "book_id", referencedColumnName = "id", nullable = true)
+    private Set<Book> books;
 }
