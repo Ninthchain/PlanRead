47255184cfb3ae3a1fa4eb70878d9e5a3785b446
Ninthchain <ninthchainOFF@gmail.com>
2025-01-26 01:30:07 +0300

    Changed findByTag method. Now it accepts set of tags to find list of tasks

--- backend/src/main/java/com/professional/backend/infrastructure/data/repository/UserTaskRepository.java
+++ backend/src/main/java/com/professional/backend/infrastructure/data/repository/UserTaskRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.professional.backend.infrastructure.data.model.dao.task.Tag;
 import com.professional.backend.infrastructure.data.model.dao.task.UserTask;
+import java.util.List;
+import java.util.Set;
+
 
 public interface UserTaskRepository extends CrudRepository<UserTask, UUID> {
     UserTask findByName(String name);
-    UserTask findByTag(Tag tag);
+    List<UserTask> findByTags(Set<Tag> tags);
+    
 }
