8cd3bc532f8627179a2efc3bbf997f3a976cb3dc
Ninthchain <ninthchainOFF@gmail.com>
2025-01-29 05:24:32 +0300

    Misc

--- backend/src/main/java/com/professional/backend/infrastructure/data/model/entity/task/Priority.java
+++ backend/src/main/java/com/professional/backend/infrastructure/data/model/entity/task/Priority.java
@@ -1,22 +1,27 @@
 package com.professional.backend.infrastructure.data.model.entity.task;
 
+import org.hibernate.annotations.SortComparator;
+import org.hibernate.annotations.SortNatural;
+import org.springframework.data.web.SortDefault;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OrderBy;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Entity(name = "priorities")
 public class Priority {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Short id;
 
+    
     @Column(name = "index")
     private Integer index;
 }
