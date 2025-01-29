bacb2204afff08b63d18b1a7d627fabf683b47a6
Ninthchain <ninthchainOFF@gmail.com>
2025-01-29 05:26:16 +0300

    Misc

--- backend/src/main/java/com/professional/backend/infrastructure/data/model/entity/task/Priority.java
+++ backend/src/main/java/com/professional/backend/infrastructure/data/model/entity/task/Priority.java
@@ -1,15 +1,9 @@
 package com.professional.backend.infrastructure.data.model.entity.task;
 
-import org.hibernate.annotations.SortComparator;
-import org.hibernate.annotations.SortNatural;
-import org.springframework.data.web.SortDefault;
-
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OrderBy;
 import lombok.Getter;
 import lombok.Setter;
 
