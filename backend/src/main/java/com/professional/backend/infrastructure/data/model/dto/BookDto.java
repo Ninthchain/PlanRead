b35387327e9e58aea04aa7522aca367ca6ea18df
Ninthchain <ninthchainOFF@gmail.com>
2025-02-02 01:53:51 +0300

    Added book dto

---
+++ backend/src/main/java/com/professional/backend/infrastructure/data/model/dto/BookDto.java
@@ -0,0 +1,13 @@
+package com.professional.backend.infrastructure.data.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BookDto {
+    private Long Id;
+    private String name;
+}
